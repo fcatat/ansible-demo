3daf58db9730418b38b75392b569b209e39b7cee
fcatat <31728829+fcatat@users.noreply.github.com>
2021-12-14 23:57:33 +0800

    update to version 3.0

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! This is dev ,version 2.0'
+    return 'Hello World! This is dev ,version 3.0'
 
 
 if __name__ == '__main__':
